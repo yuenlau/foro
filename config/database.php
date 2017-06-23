98d94c5e63617e04234030329078cd16be5d6981
Jose Ramón Lea Otero <jleasg3@gmail.com>
2017-06-23 18:40:47 +0200

    Fix database configuration for Codeship

--- config/database.php
+++ config/database.php
@@ -62,11 +62,11 @@ return [
 
         'mysql_tests' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE_TEST', 'foro_tests'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE_TESTS', 'foro_tests'),
+            'username' => env('MYSQL_USER', env('DB_USERNAME', 'forge')),
+            'password' => env('MYSQL_PASSWORD', env('DB_PASSWORD', '')),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
