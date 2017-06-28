7752b3105bea285004a450c5d65721ecd3d3225c
Jose Ramón Lea Otero <jleasg3@gmail.com>
2017-06-28 21:01:29 +0200

    Leccion 14: Verificar features ya desarrollados usando pruebas de regresión en Laravel

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ShowPostTest extends TestCase
 {
 
-    public function test_a_user_can_see_the_post_details()
+    function test_a_user_can_see_the_post_details()
     {
         // Having
         $user = $this->defaultUser([
@@ -25,9 +25,56 @@ class ShowPostTest extends TestCase
         $user->posts()->save($post);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->content)
             ->see($user->name);
     }
+
+    function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $this->actingAs($user);
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->seePageIs($post->url);
+    }
+
+/*
+    function test_post_url_with_wrong_slugs_still_work()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $this->actingAs($user);
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->assertResponseOk()
+            ->see('New title');
+
+    }
+*/
+
 }
--- tests/integration/PostIntegrationTest.php
+++ tests/integration/PostIntegrationTest.php
@@ -8,6 +8,20 @@ class PostIntegrationTest extends TestCase
 
     use DatabaseTransactions;
 
+    public function test_return_correct_url_post()
+    {
+        $user = $this->defaultUser();
+        $post = factory(Post::class)->make();
+
+        $user->posts()->save($post);
+
+        $url_true = env('APP_URL') . "/posts/" . $post->id . "-" . $post->slug;
+        $url = $post->url;
+
+        $this->assertSame($url, $url_true);
+
+    }
+
     public function test_a_slug_is_generated_and_saved_to_the_database()
     {
         $user = $this->defaultUser();
